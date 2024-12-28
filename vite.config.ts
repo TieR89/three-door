ffa3914b87512f82b2073521b7324799edac3498
Anton <tier8@yandex.ru>
2024-12-28 20:06:56 +0300

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   base: process.env.NODE_ENV === 'production' ? '/three-door/' : '/',
+// })
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/three-door/' : '/',
+  base: '/three-door/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
